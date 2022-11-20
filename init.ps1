635308a9066a2cc3fa0fdda6b275eec7ab52b805
Yamazaki Takeshi <yamazaki@uhc.jp>
2022-11-20 22:10:30 +0900

    first commit

---
+++ init.ps1
@@ -0,0 +1,10 @@
+New-Item mysql/data -ItemType Directory
+New-Item laravel/html -ItemType Directory
+#Set-ItemProperty mysql/my.cnf -Name IsReadOnly -Value $true
+docker-compose up -d --build
+docker-compose exec laravel git clone --depth 1 https://github.com/laravel/laravel.git .
+docker-compose exec laravel rm -rf .git
+docker-compose exec laravel rm -rf .github
+docker-compose exec laravel composer install
+docker-compose exec laravel chmod -R 777 storage
+Remove-Item init.ps1
