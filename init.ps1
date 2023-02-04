987d64c447a997842f9544c2029a0744fe6b41d4
Yamazaki Takeshi <yamazaki@uhc.jp>
2023-02-04 18:28:28 +0900

    fix

--- init.ps1
+++ init.ps1
@@ -1,12 +1,12 @@
-New-Item mysql/data -ItemType Directory
-New-Item laravel/html -ItemType Directory
-Set-ItemProperty mysql/my.cnf -Name IsReadOnly -Value $true
+New-Item db/data -ItemType Directory
+New-Item src/laravel -ItemType Directory
+Set-ItemProperty db/my.cnf -Name IsReadOnly -Value $true
 docker-compose up -d --build
-docker-compose exec laravel git clone --depth 1 https://github.com/laravel/laravel.git .
-docker-compose exec laravel rm -rf .git
-docker-compose exec laravel rm -rf .github
-docker-compose exec laravel composer install
-docker-compose exec laravel chmod -R 777 storage
+docker-compose exec app git clone --depth 1 https://github.com/laravel/laravel.git .
+docker-compose exec app rm -rf .git
+docker-compose exec app rm -rf .github
+docker-compose exec app composer install
+docker-compose exec app chmod -R 777 storage
 Remove-Item init.ps1
 Remove-Item .git -Recurse -Force
 git init
