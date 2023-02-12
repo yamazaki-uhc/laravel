709784d190b8c91028a20df09e8163e6a52ee1ec
Yamazaki Takeshi <yamazaki@uhc.jp>
2023-02-12 16:44:27 +0900

    fix

--- init.ps1
+++ init.ps1
@@ -3,21 +3,23 @@ $PHP_VERSION="8.2.2"
 $LARAVEL_VERSION="10"
 # ----------------
 
-echo "PHP_VERSION=$PHP_VERSION" > .env
+echo "FROM php:$PHP_VERSION-apache" > src/dockerfile/Dockerfile
+cat src/dockerfile/Dockerfile_template >> src/dockerfile/Dockerfile
+rm src/dockerfile/Dockerfile_template
 New-Item db/data -ItemType Directory
-New-Item src/laravel -ItemType Directory
 Set-ItemProperty db/my.cnf -Name IsReadOnly -Value $true
 docker-compose up -d --build
-docker-compose exec app git clone --depth 1 -b "$LARAVEL_VERSION.x" https://github.com/laravel/laravel.git .
-docker-compose exec app rm -rf .git
-docker-compose exec app rm -rf .github
+docker-compose exec app git clone --depth 1 -b "$LARAVEL_VERSION.x" https://github.com/laravel/laravel.git tmp
+docker-compose exec app bash init.sh
 docker-compose exec app composer install
 docker-compose exec app chmod -R 777 storage
 docker-compose exec app composer require --dev barryvdh/laravel-debugbar
+docker-compose exec app npm i
 Remove-Item init.ps1
-Remove-Item .env
 Remove-Item .git -Recurse -Force
 Move-Item .vscode src/laravel
+Move-Item src/laravel/vite.config.js src/laravel/vite.config.js_bk
+Move-Item src/laravel/vite.config.js_template src/laravel/vite.config.js
 git init
 git add .
 git commit -m "first commit"
---
+++ src/laravel/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mv tmp/* ./
+mv tmp/.* ./
+rm -rf tmp
+rm -rf .git
+rm -rf .github
+rm init.sh
