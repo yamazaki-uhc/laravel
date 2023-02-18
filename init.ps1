4716185be1c3bc108a116331fe1220f82f1d735f
Yamazaki Takeshi <yamazaki@uhc.jp>
2023-02-18 17:57:03 +0900

    fix

--- init.ps1
+++ init.ps1
@@ -18,10 +18,6 @@ Set-ItemProperty db/my.cnf -Name IsReadOnly -Value $true
 docker-compose up -d --build
 docker-compose exec app git clone --shallow-exclude "v$LARAVEL_VERSION.0.0" -b "$LARAVEL_VERSION.x" https://github.com/laravel/laravel.git tmp
 docker-compose exec app bash init.sh
-docker-compose exec app composer install
-docker-compose exec app chmod -R 777 storage
-docker-compose exec app composer require --dev barryvdh/laravel-debugbar barryvdh/laravel-ide-helper
-docker-compose exec app npm i
 Remove-Item init.ps1
 Remove-Item .git -Recurse -Force
 Move-Item .vscode src/laravel
--- src/laravel/init.sh
+++ src/laravel/init.sh
@@ -3,6 +3,10 @@
 mv tmp/* ./
 mv tmp/.* ./
 rm -rf tmp
-rm init.sh
 git remote rename origin laravel
 git switch -c main
+composer install
+chmod -R 777 storage
+composer require --dev barryvdh/laravel-debugbar barryvdh/laravel-ide-helper
+npm i
+rm init.sh
