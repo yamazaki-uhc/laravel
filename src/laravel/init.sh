56c875093af08bdc1085909a6ea0a8d4edc50430
Yamazaki Takeshi <yamazaki@uhc.jp>
2023-02-19 21:04:08 +0900

    fix

--- src/laravel/init.sh
+++ src/laravel/init.sh
@@ -6,7 +6,7 @@ rm -rf tmp
 git remote rename origin laravel
 git switch -c main
 composer install
-chmod 777 storage/log
+chmod 777 storage/logs
 composer require --dev barryvdh/laravel-debugbar barryvdh/laravel-ide-helper
 npm i
 rm init.sh
